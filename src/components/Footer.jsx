b7d7f95aaacf7991feaec4291d4d0b88cfdeb1bc
Tanya Damgaard <damgaardtanya@gmail.com>
2022-08-29 17:45:33 +0200

    fixed components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="App-footer">
+        <a className="App-link" href="https://github.com/halmur/Projekt-Agil" target="_blank" rel="noopener noreferrer">Meet the projekt</a>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (      
+    <> 
+        <main className="App-main">
+        <p> Projekt Agil (Scrum) Webbutveckling i JavaScript </p>
+        </main>
+    </>
+  )
+}
+
+export default Main
