99c3ebc5fc5ea7a48e42e35b6a98e4169c7fa7ff
BoostedPanda <kevinujfalusi@hotmail.se>
2022-08-30 10:13:29 +0200

    Added new logo and cleaned up css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from '../media/coolfashion.PNG';
+import logo from '../media/logo.svg'
+import logo2 from '../media/logo_w_space.svg'
 import Navbar from './Navbar';
 
 const Header = () => {
@@ -7,10 +8,7 @@ const Header = () => {
     <> 
         <header className = "App-header" >
             <div className='nav-area'>
-        {/* <img src={logo} className="App-logo" alt="logo"/> */}
-        <a href="/" className="logo">
-          CoolFashion
-        </a>
+        <img src={logo2} className="App-logo" alt="logo"/>
         <Navbar/>
         </div>
         </header>
